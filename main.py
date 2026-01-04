443e70e21c83b273f3e931dad592921cb0010170
aash1kkk <aashikmohammed700@gmail.com>
2026-01-04 13:15:46 +0530

    signing key added

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from vecotr_db import QdrantStorage
 from customs_types import RAQQueryResult, RAGSearchResult ,RAGUpsertResult,RAGChunkAndSrc
 from google import genai
 load_dotenv()
+signing_key = os.getenv("INNGEST_SIGNING_KEY")
 app = FastAPI()
 inngest_client = inngest.Inngest(
     app_id="rag_app",
